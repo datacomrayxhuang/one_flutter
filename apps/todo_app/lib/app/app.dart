688052f5203c80b6551e466eabe710277638bdd3
Ray <rayxhuang2@gmail.com>
2025-04-15 21:36:21 +1200

    Melos set up

---
+++ apps/todo_app/lib/app/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/screens/home.dart';
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ apps/todo_app/lib/screens/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Home'),
+      ),
+      body: const Placeholder(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Add To-Do',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
